612f9cce464d51bf93d488f91d26f09d40582194
Dev <devenlukeday@gmail.com>
2020-09-23 16:06:23 -0600

    initial commit

---
+++ SwapiAPIAPP/SwapiAPIAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwapiAPIAPP
+//
+//  Created by Deven Day on 9/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
